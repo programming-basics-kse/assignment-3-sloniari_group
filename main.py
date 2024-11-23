c4e7cfcfcfbda02efce659cacc93577cc59aa701
Taburetochka <stiv.kimmel05@gmail.com>
2024-11-23 18:06:12 +0200

    pre_made

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import warnings
 import sys
 
+save_file = False
 if 5 < len(sys.argv) < 7:
     print("Error: Incorrect input argument.")
     sys.exit(1)
-if sys.argv[2] != "-medals":
+
+if sys.argv[1] != "-medals":
     print("Error: The '-medals' argument must be the second argument.")
     sys.exit(1)
 
-country_name = sys.argv[3]
-year_name = sys.argv[4]
+if len(sys.argv) == 6 and sys.argv[4] == '-output':
+    save_file = True
+    save_path = sys.argv[5]
 
+country_name = sys.argv[2]
+year_name = int(sys.argv[3])
+medals =["Gold", "Silver", "Bronze"] 
 
-file_path = "assignment-3-sloniari_group\side\athlete_events.csv"
-
+file_path = "./side/athlete_events.csv"
 file = pd.read_csv(file_path)
 
 df = file[file["NOC"] == country_name]
-print(df)
 df_year = df[df["Year"] == year_name]
-print(df_year)
-arg = file["Team"].unique()
+df_medals = df_year[df_year['Medal'].isin(medals)]
+
+print(df_medals['Medal'].value_counts())
+for i in 
+
 
 # with open('text.txt', 'w') as file:
 #     for country in arg:
