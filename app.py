62ec555d8f770a0f3ae547c5d38809e44f3a86f2
Volodymyr Kustikov <kustforever@gmail.com>
2024-11-28 13:01:57 +0200

    me2

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
 import pandas as pd
+import sys
+
+print(sys.argv[0])
+
+def totalProcess():
+  
+  if(sys.argv[1] == "-total" 
+  and int(sys.argv[2]) >= 1896
+  and int(sys.argv[2]) <= 2016):
+    year = int(sys.argv[2])
+    medals =["Gold", "Silver", "Bronze"]
+    
+    file_path = './dataSet/data.csv'
+    file = pd.read_csv(file_path)
+    
+    df_year = file[file["Year"] == year]
+    df_medals = df_year[df_year['Medal'].isin(medals)]
+    df_countries = df_medals[df_medals['Team']]
+    print(df_countries)
+    
+totalProcess()
+
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import warnings
 import sys
 
 save_file = False
+
 if 5 < len(sys.argv) < 7:
     print("Error: Incorrect input argument.")
     sys.exit(1)
@@ -20,23 +21,16 @@ country_name = sys.argv[2]
 year_name = int(sys.argv[3])
 medals =["Gold", "Silver", "Bronze"] 
 
-file_path = "./side/athlete_events.csv"
+file_path = "./dataSet/data.csv"
 file = pd.read_csv(file_path)
 
 df = file[file["NOC"] == country_name]
-df_year = df[df["Year"] == year_name]
+df_year = df[df["Year"] == year_name]   
 df_medals = df_year[df_year['Medal'].isin(medals)]
 
 print(df_medals['Medal'].value_counts())
-for i in 
+df_medalists = df_medals[["Name", "Sport", "Medal"]].reset_index(drop=True)
+print(df_medalists.head(10))
 
-
-# with open('text.txt', 'w') as file:
-#     for country in arg:
-#         file.write(country +"\n" )
-
-# parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-# parser.add_argument("-c", "--country", default="SE", help="Two-letter country code")
-# parser.add_argument("-l", "--length", default=40, type=int, help="Length of time series to fit the ARIMA model")
-# parser.add_argument("-s", "--start", default=0, type=int, help="Starting offset to fit the ARIMA model")
-# args = vars(parser.parse_args())
+list_med = [df_medalists["Name"].tolist()] + [df_medalists["Sport"].tolist()] + [df_medalists["Medal"].tolist()]
+print(list_med[0][0], list_med[1][0], list_med[2][0])
